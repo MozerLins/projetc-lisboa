645826b00b46e094701f8a63f81789009da28904
MozerLins <suimozer@gmail.com>
2022-05-19 22:52:20 -0300

    Menu Lateral

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 var myCarousel = document.querySelector('#carousel-index')
+var MenuDropdown = document.querySelector('#CollapseMenu')
+var triggerTabList = document.getElementById('usermenu')
+
+QuerySelector();
 
 function myCarouse(){
     var carousel = new bootstrap.Carousel(myCarousel, {
@@ -6,3 +10,25 @@ function myCarouse(){
       wrap: true
     })
 }
+
+
+
+/*
+function MenuDropdown(){
+  const bsCollapse = new bootstrap.Collapse('#myCollapse', {
+    toggle: false
+  })
+}
+
+function QuerySelector(){
+  triggerTabList.addEventListener("click", function(event) {
+     menuselect = event.target.innerHTML;
+     const triggerEl =document.querySelector('#usermenu').innerHTML = menuselect
+     
+     triggerEl.addEventListener('shown.bs.tab', event => {
+      event.target // newly activated tab
+      event.relatedTarget // previous active tab
+    })
+ })
+ 
+}*/
